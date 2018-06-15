5f9c2ea20c85eadc7b8a153456aaacec3d4b896b
Sachintha <ravindusachintha53@gmail.com>
2018-06-15 09:42:12 +0530

    update config.php file

---
+++ inc/config.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Database connection building module */
+
+$host=""; 		// default "localhost"
+$username=""; 	// default "root"
+$password=""; 	// default ""
+$db="";	// use the database name
+
+$connection=mysqli_connect($host,$username,$password,$db);
+
+?>
